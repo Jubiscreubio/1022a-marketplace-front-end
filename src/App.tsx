f7222d5331b6e1c42dfbed3a282b656806f0e281
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2024-10-15 10:36:01 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,44 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 type ProdutoType = {
-  id:number,
-  nome:string,
-  preco:string,
-  descricao:string,
-  imagem:string
+    id:number,
+    nome:string,
+    preco:string,
+    descricao:string,
+    imagem:string
 }
 function App() {
   const [nome, setNome] = useState("")
   const [produtos, setProdutos] = useState<ProdutoType []>([])
-  //useEffects(O que fazer, quando fazer) []=> Hora do carregamento da pagina
-  useEffect(()=>{
-    setNome("Zezio") 
-    //Buscar os dados do BackEnd
-    fetch("https://one022a-marketplace.onrender.com/produtos")
-    .then(resposta=>resposta.json())
-    .then(dados=>setProdutos(dados))
-    //Colocar em uma variavel
-  },[])
-
+  //useEffects(O que fazer, quando Fazer) []=> Hora do carregamento da página
+  useEffect(() => {
+    setNome("Zezio")
+    //Buscar os dados do BackENd
+    fetch("https://one022a-marketplace-e90o.onrender.com/produtos")
+      .then(resposta => resposta.json())
+      .then(dados => setProdutos(dados))
+    //Colocar em uma variável
+  }, [])
 
   return (
-  <>
-  <h1>{nome}</h1>
-  <div className="produtos-container">
-    {
-      produtos.map(produto=>{
-        return(
-          <div key ={produto.id}className="produto-item">
-      <h1>{produto.nome}</h1>
-      <img src={produto.imagem} alt=
-      <p>{produto.preco}</p>
-      <p>{produto.descricao}</p>
-    </div>
-        )
-      })
-    }
-    
-  </div>
-  </>
+    <>
+      <h1>{nome}</h1>
+      <div className="produtos-container">
+        {
+          produtos.map(produto => {
+            return (
+              <div key={produto.id} className="produto-item">
+                <h1>{produto.nome}</h1>
+                <img src={produto.imagem} alt="Imagem do celular" />
+                <p>{produto.preco}</p>
+                <p>{produto.descricao}</p>
+              </div>
+            )
+          })
+        }
+
+      </div>
+    </>
   )
 }
 
