e1d62e3ffc4ce5c696db0e1835a295cd04dea69e
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2024-11-05 11:50:12 -0400

    a

---
+++ src/componentes/cadastroproduto/CadastroProduto.tsx
@@ -0,0 +1,41 @@
+import {  ChangeEvent, FormEvent, useState } from "react"
+
+function CadastroProduto(){
+    const [id,setId] = useState("")
+    function handleForm(event:FormEvent){
+        event.preventDefault()
+        console.log("Cliquei em cadastrar")
+        //Pegar os valores dos campos
+        //Mandar pelo fetch para o Back-End
+    }
+    function handleId(event:ChangeEvent<HTMLInputElement>){
+        setId(event.target.value)
+    }
+    return( 
+        <> 
+        <h1>Meu Componente de Cadastro de Produtos</h1>
+        <form onSubmit={handleForm}>
+           <div>
+            <input placeholder="Id" type="text" name="id" id="id" onClick={handleId}/>
+            </div>
+           <div>
+            <input placeholder="Nome" type="text" name="nome" id="nome" />
+            </div>
+           <div>
+            <input placeholder="Descrição" type="text" name="descricao" id="descrição" />
+            </div>
+           <div>
+            <input placeholder="Preço" type="text" name="preco" id="preco" />
+            </div>
+           <div>
+            <input placeholder="Imagem" type="text" name="imagem" id="imagem" />
+            </div>
+            <input type="submit" value="Cadastrar" />
+        </form></>
+       
+        )
+}
+
+
+
+export default  CadastroProduto
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import CadastroProduto from './componentes/cadastroproduto/CadastroProduto.tsx';
 
-createRoot(document.getElementById('root')!).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path: "/cadastro-produto",
+    element: <CadastroProduto/>
+  }
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
+
