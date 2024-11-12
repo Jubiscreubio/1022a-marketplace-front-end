a2ea571505adf9742cb2e9544246329eb5be04c6
Vinicius Azevedo <118370027+Jubiscreubio@users.noreply.github.com>
2024-11-12 10:26:14 -0400

    Update CadastroProduto.tsx

--- src/componentes/cadastroproduto/CadastroProduto.tsx
+++ src/componentes/cadastroproduto/CadastroProduto.tsx
@@ -11,31 +11,29 @@ function CadastroProduto(){
     function handleId(event:ChangeEvent<HTMLInputElement>){
         setId(event.target.value)
     }
-    return( 
-        <> 
-        <h1>Meu Componente de Cadastro de Produtos</h1>
-        <form onSubmit={handleForm}>
-           <div>
-            <input placeholder="Id" type="text" name="id" id="id" onClick={handleId}/>
-            </div>
-           <div>
-            <input placeholder="Nome" type="text" name="nome" id="nome" />
-            </div>
-           <div>
-            <input placeholder="Descrição" type="text" name="descricao" id="descrição" />
-            </div>
-           <div>
-            <input placeholder="Preço" type="text" name="preco" id="preco" />
-            </div>
-           <div>
-            <input placeholder="Imagem" type="text" name="imagem" id="imagem" />
-            </div>
-            <input type="submit" value="Cadastrar" />
-        </form></>
-       
-        )
+    return(
+        <>
+            <h1>Meu Componente de Cadastro de Produtos</h1>
+            <form onSubmit={handleForm}>
+                <div>
+                    <input placeholder="Id" type="text" name="id" id="id" onChange={handleId} />
+                </div>
+                <div>
+                    <input placeholder="Nome" type="text" name="nome" id="nome" />
+                </div>
+                <div>
+                    <input placeholder="Descrição" type="text" name="descricao" id="descricao" />
+                </div>
+                <div>
+                    <input placeholder="Preço" type="text" name="preco" id="preco" />
+                </div>
+                <div>
+                    <input placeholder="URL Imagem" type="text" name="imagem" id="imagem" />
+                </div>
+                <input type="submit" value="Cadastrar" />
+            </form>
+        </>
+    )
 }
 
-
-
-export default  CadastroProduto
+export default CadastroProduto
